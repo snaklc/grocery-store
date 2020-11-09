811e11148c5df32a9b605642080a0c873843ffd1
snaklc <senakilic3@gmail.com>
2020-11-09 13:38:25 +0300

    hatalar duzeltildi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,79 +14,104 @@ export class AppComponent {
       name: 'apple',
       alias: 'apple',
       price: 25,
-      image: '/assets/apple.gif'
+      image: '/assets/apple.gif',
+      number: 1
     },
     {
       name: 'lemon',
       alias: 'lemon',
       price: 5,
-      image: '/assets/lemon.jpg'
+      image: '/assets/lemon.jpg',
+      number: 1
+
     },
     {
       name: 'grapes',
       alias: 'grapes',
       price: 14,
-      image: '/assets/grapes.png'
+      image: '/assets/grapes.png',
+      number: 1
+
     },
     {
       name: 'passionfruit',
       alias: 'passionfruit',
       price: 32,
-      image: '/assets/passion-fruit.png'
+      image: '/assets/passion-fruit.png',
+      number: 1
+
     },
     {
       name: 'rasberry',
       alias: 'rasberry',
       price: 7,
-      image: '/assets/raspberry.png'
+      image: '/assets/raspberry.png',
+      number: 1
+
     },
     {
       name: 'watermelon',
       alias: 'watermelon',
       price: 17,
-      image: '/assets/watermelon.jpg'
+      image: '/assets/watermelon.jpg',
+      number: 1
+
     },
     {
       name: 'orange',
       alias: 'orange',
       price: 12,
-      image: '/assets/orange.jpg'
+      image: '/assets/orange.jpg',
+      number: 1
+
     },
     {
       name: 'strawberry',
       alias: 'strawberry',
       price: 14,
-      image: '/assets/strawberry.png'
+      image: '/assets/strawberry.png',
+      number: 1
+
     },
     {
       name: 'kiwi',
       alias: 'kiwi',
       price: 3,
-      image: '/assets/kiwi.png'
+      image: '/assets/kiwi.png',
+      number: 1
+
     },
     {
       name: 'cherry',
       alias: 'cherry',
       price: 6,
-      image: '/assets/cherry.png'
+      image: '/assets/cherry.png',
+      number: 1
+
     },
     {
       name: 'carrot',
       alias: 'carrot',
       price: 5,
-      image: '/assets/carrot.png'
+      image: '/assets/carrot.png',
+      number: 1
+
     },
     {
       name: 'avocado',
       alias: 'avocado',
       price: 5,
-      image: '/assets/avocado.png'
+      image: '/assets/avocado.png',
+      number: 1
+
     },
     {
       name: 'pineapple',
       alias: 'pineapple',
       price: 5,
-      image: '/assets/pineapple.png'
+      image: '/assets/pineapple.png',
+      number: 1
+
     },
   ]
 
@@ -94,23 +119,25 @@ export class AppComponent {
   selectedFruits = [];
   sum = 0;
   obj;
-
   ngOnInit(): void {
 
 
   }
 
   clicktedElement(event) {
+    let bulunan = this.selectedFruits.find((findedFruit) => {
+      return findedFruit.name === event.name;
 
-    
-    this.selectedFruits.find((findedFruit) => {
-      if (findedFruit.name === event.name) { 
-        console.log('Sepette var!!');
-      } 
     })
-    this.selectedFruits.push(event);
-    this.sum = this.sum + event.price;
 
+    if (!bulunan) {
+      this.selectedFruits.push(event);
+    }
+    else{
+    console.log(bulunan.number++);
+
+    }
+    this.sum = this.sum + event.price;
   }
 
 
